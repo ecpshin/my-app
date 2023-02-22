df2ee016cea42b02e3b64d7a6a4c4db880a9007d
epcshin <francisco.shin@hotmail.com>
2023-02-22 08:46:03 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { createClient } from '@supabase/supabase-js'
+import { useEffect, useState } from 'react';
+import api from './services/api';
 
 function App() {
+
+  const [paises, setPaises] = useState([]);
+
+  const supabase = createClient(api.url, api.api_key);
+
+  useEffect(()=>{
+    async function getCountries() {
+      const countries = await supabase.from('countries').select()
+      setPaises(countries.data)
+    }
+    getCountries();
+  }, [supabase]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        paises.map((pais) => <p key={pais.id}>{pais.name}</p>)
+      }
     </div>
   );
 }
